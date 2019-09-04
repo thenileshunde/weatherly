a6a5eaf0e2b3cc9ce2674419cbdc05d249284c26
Slash Caret <slashslashcaret@gmail.com>
2019-09-04 16:38:41 +0530

    Added Code to delete a city from database and added Footer

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'qwertyuiopsecretkey'
 
 db = SQLAlchemy(app)
 
@@ -15,46 +19,82 @@ class City(db.Model):
 
 # app.config['DEBUG'] = True
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    key = 'c9f1f34212b6ce232a81ca55ffc01e4f'
-    unit = 'metric'
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid={}'
-    
-    not_found_flag = 0
-    
-    if request.method == 'POST':
-        new_city = request.form.get('city').strip()
-        
-        res = requests.get(url.format(new_city, unit, key)).json()
-        
-        if res['cod'] == 200 and new_city:
-            city_to_add = City(name=new_city)
-            db.session.add(city_to_add)
-            db.session.commit()
-        elif(new_city == ''):
-            not_found_flag = 0
-        else:
-            not_found_flag = 1
-    
+key = 'c9f1f34212b6ce232a81ca55ffc01e4f'
+unit = 'metric'
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units={}&appid={}'
+
+
+def get_weather_data(city):
+    r = requests.get(url.format(city, unit, key)).json()
+    return r
+
+
+@app.route('/')
+def index_get():
     cities = City.query.all()
     
     weather_data = []
     
+    
     for city in cities:
-        r = requests.get(url.format(city.name, unit, key)).json()
+        r = get_weather_data(city.name)
         # print(r)
         #
         weather = {
             'city': city.name,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon']
+            'temperature': round(r['main']['temp']),
+            'description': str(r['weather'][0]['description']).title(),
+            'icon': r['weather'][0]['icon'],
+            'humidity':r['main']['humidity'],
+            'wind': r['wind']['speed']
         }
         
         weather_data.append(weather)
+        
+        
     
-    return render_template('index.html', weather_data=weather_data, flag=not_found_flag)
+    return render_template('index.html', weather_data=weather_data)
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    err_msg = ''
+    
+    new_city = request.form.get('city').strip().lower()
+    
+    res = get_weather_data(new_city)
+    
+    if res['cod'] == 200 and new_city:
+        
+        city_exists = City.query.filter(City.name.ilike(f"%{new_city}%")).first()
+        
+        if not city_exists:
+            
+            city_to_add = City(name=new_city.title())
+            db.session.add(city_to_add)
+            db.session.commit()
+        else:
+            err_msg = 'City Already Exists'
+    else:
+        err_msg = 'City does not exist in the world !'
+    
+    if err_msg:
+        flash(err_msg, 'error')
+    else:
+        flash('City Added Successfully !', 'success')
+    
+    return redirect(url_for('index_get'))
+
+
+@app.route('/delete/<name>')
+def delete_city(name):
+    city = City.query.filter_by(name=name).first()
+    db.session.delete(city)
+    db.session.commit()
+    
+    flash('Successfully Deleted {}'.format(city.name),'success')
+    
+    return redirect(url_for('index_get'))
 
 
 if __name__ == "__main__":
